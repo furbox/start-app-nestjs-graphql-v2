7419a94bc65008df0cad986598a765f5e5659052
furbox <furbox@gmail.com>
2022-07-04 00:55:38 -0500

    add fastify

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,36 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { AppResolver } from './app.resolver';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver } from '@nestjs/apollo';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      connectionFactory: (connection) => {
+        // eslint-disable-next-line @typescript-eslint/no-var-requires
+        connection.plugin(require('mongoose-autopopulate'));
+        return connection;
+      },
+    }),
+    GraphQLModule.forRoot({
+      autoSchemaFile: 'schema.gql',
+      driver: ApolloDriver,
+      debug: false,
+    }),
+  ],
+  providers: [AppService, AppResolver],
 })
-export class AppModule {}
+export class AppModule {
+  static port: number | string;
+  constructor() {
+    AppModule.port = process.env.PORT;
+  }
+}
